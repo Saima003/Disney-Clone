6c4ba0eebcb476cd100211ef83b3b33a718196f7
Saima003 <saima19@somaiya.edu>
2024-03-31 19:51:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import './tailwind.css'
+function App() {
+  return (
+    <div className="App">
+      <h2 className='pl-4 '>Disney+++</h2>
+    </div>
+  );
+}
+
+export default App;
