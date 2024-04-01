9b171f06a8e2bf2b03deaea229d7bf5b3ccb7bee
Saima003 <saima19@somaiya.edu>
2024-04-01 19:40:27 +0530

    Login page and Content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+// import { Routes ,Route } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from './components/Login'
+import About from './components/About';
 // import './tailwind.css'
 function App() {
   return (
     <div className="App">
-      <h2 className='pl-4 '>Disney+++</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/about' element={<About/>}/>
+          {/* <Login/>/ */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from "./Login.module.css"
+const Login = () => {
+  return (
+    <>
+    <section className="overflow-hidden flex flex-col h-screen text-center">
+      <div className= {`${classes['mb-custom']} w-full h-screen relative min-h-screen box-border flex justify-center items-center flex-col`}>
+        Content
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Login
