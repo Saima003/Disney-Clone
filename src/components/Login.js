858a7249ff5efff095de5d64b902f8cbbbb908d6
Saima003 <saima19@somaiya.edu>
2024-04-05 23:00:37 +0530

    Added Small Logos

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from "./Login.module.css"
 import bgImage from '../images/login-background.jpg'
 import logoImg from "../images/cta-logo-one.svg"
+import smallLogo from "../images/cta-logo-two.png"
 const Login = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const Login = () => {
           <img className='mb-10 max-w-600 min-h-1 w-full block' src={logoImg} alt="" />
           <a className={`${classes['get-all']}`}>GET ALL THERE</a>
           <p className={`${classes['para-style']}`}>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/04/24, the price of Disney+ and the Disney Bundle will increase by $1.</p>
+          <img className={`${classes['small-logo']}`} src={smallLogo} alt=''/>
         </div>
           <img className="h-100 bg-top bg-cover absolute top-0 left-0 right-0 z-[-1]" src={bgImage} alt="" />
          {/* </div> */}
